742e3cf8f76d7ea63e372dd5ddaf1891750a8fa2
hreinn <hreinngy@gmail.com>
2022-11-24 14:19:40 +0100

    Adds link to element and hover states, and adds placeholder title on listings which have none.

--- src/js/index.js
+++ src/js/index.js
@@ -9,21 +9,24 @@ getListings(API_BASE_URL + GET_LISTINGS)
   .then(response => {
     const html = response.map(({id, media, title, bids}) => {
       let listingImg = media[0]
-      if (!listingImg) {
-        listingImg = listingPlaceholderImg
-      }
+      !listingImg ? listingImg = listingPlaceholderImg : null
+
       let currentBid = 'NO BIDS'
       if (bids.length) {
         const highestBid = Math.max(...bids.map(bid => bid.amount))
         currentBid = `${highestBid} Credits`
       }
-      return `<a href="listing-details.html?id=${id}">
+
+      let titleOFListing = title
+      !titleOFListing ? titleOFListing = 'No Title' : null
+
+      return `<a href="listing-details.html?id=${id}" class="group">
                 <div class="flex flex-col gap-4">
                   <div class="h-64 w-full bg-gray-800 bg-cover bg-center"
                        style="background-image: url(${listingImg})"></div>
-                  <h3 class="text-xl font-medium">${title}</h3>
+                  <h3 class="text-xl font-medium">${titleOFListing}</h3>
                   <p>Current Bid: <span class="text-emerald-700 font-bold">${currentBid}</span></p>
-                  <div class="bg-blue-700 text-white text-center w-full rounded py-2">View Details</div>                 
+                  <div class="bg-blue-700 text-white text-center w-full rounded py-2 group-hover:bg-blue-600">View Details</div>                 
                 </div>
               </a>`
     })
