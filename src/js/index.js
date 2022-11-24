1dcc47afe694cabd0ab817bbf9529e56e581cc46
hreinn <hreinngy@gmail.com>
2022-11-24 11:40:13 +0100

    Adds links to nav elements and fixes html errors

--- src/js/index.js
+++ src/js/index.js
@@ -17,13 +17,13 @@ getListings(API_BASE_URL + GET_LISTINGS)
         const highestBid = Math.max(...bids.map(bid => bid.amount))
         currentBid = `${highestBid} Credits`
       }
-      return `<a href="listing-details.html?id=${id}"
+      return `<a href="listing-details.html?id=${id}">
                 <div class="flex flex-col gap-4">
                   <div class="h-64 w-full bg-gray-800 bg-cover bg-center"
                        style="background-image: url(${listingImg})"></div>
                   <h3 class="text-xl font-medium">${title}</h3>
                   <p>Current Bid: <span class="text-emerald-700 font-bold">${currentBid}</span></p>
-                  <button class="bg-blue-700 text-white w-full h-10 rounded">View Details</button>
+                  <div class="bg-blue-700 text-white text-center w-full rounded py-2">View Details</div>                 
                 </div>
               </a>`
     })
