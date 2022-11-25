03e488ded4516f10122e777d18aee10906e53155
hreinn <hreinngy@gmail.com>
2022-11-25 20:44:30 +0100

    Cleans up code in createNavHeaderBar

--- src/js/components/createNavHeaderBar.js
+++ src/js/components/createNavHeaderBar.js
@@ -5,20 +5,17 @@ export function createNavHeaderBar() {
   const navItems = document.querySelector('#menu')
   const searchProfileContainer = document.querySelector('#search-profile-container')
 
-  const profileMobile =
+  const profileIcon =
     `<a href="profile.html" title="Profile" class="sm:order-2">
-       <div class="h-8 w-8 rounded-full mr-3 bg-center bg-cover"
-            style="background-image: url(${profileImgWhite})"></div>
+       <div class="h-8 w-8 rounded-full mr-3 bg-center bg-cover" style="background-image: url(${profileImgWhite})"></div>
      </a>`
 
   const navItemsNotSignedIn =
     `<li class="mt-8 font-medium sm:hidden">
-       <a href="sign-up.html"
-        class="block bg-amber-400 text-black text-center w-full rounded-md py-2 hover:brightness-110">Sign Up</a>
+       <a href="sign-up.html" class="block bg-amber-400 text-black text-center w-full rounded-md py-2 hover:brightness-110">Sign Up</a>
      </li>
      <li class="mt-8 font-medium sm:hidden">
-       <a href="sign-in.html"
-          class="block bg-blue-700 text-white text-center w-full rounded-md py-2 hover:bg-blue-600">Sign In</a>
+       <a href="sign-in.html" class="block bg-blue-700 text-white text-center w-full rounded-md py-2 hover:bg-blue-600">Sign In</a>
      </li>`
 
   const navItemsSignedIn =
@@ -30,10 +27,10 @@ export function createNavHeaderBar() {
      </li>`
 
   if (!placeHolderLocalstorage) {
-    navItems.innerHTML += navItemsNotSignedIn
-  } else {
     navItems.innerHTML += navItemsSignedIn
-    searchProfileContainer.insertAdjacentHTML('afterbegin', profileMobile)
+    searchProfileContainer.insertAdjacentHTML('afterbegin', profileIcon)
     document.querySelector('#sign-up-log-in-desktop').style.display = 'none'
+  } else {
+    navItems.innerHTML += navItemsNotSignedIn
   }
 }
