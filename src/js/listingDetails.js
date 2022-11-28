09c3f9428c54590bde05f14ac79599b2fe953489
hreinn <hreinngy@gmail.com>
2022-11-28 13:27:01 +0100

    Adds listing details page JS, minus date

--- src/js/listingDetails.js
+++ src/js/listingDetails.js
@@ -1,8 +1,54 @@
-const mainListingImg = document.querySelector('#listing-img-main')
-const galleryImgs = document.querySelectorAll('.gallery-img')
+import {API_BASE_URL, GET_LISTING_DETAILS} from "./settings/api";
+import {getListings} from "./settings/getListings";
+import listingPlaceholderImg from "../img/placeholder-image.svg";
+import {showErrorMsg} from "./utils/errorMessages";
 
-galleryImgs.forEach((item) => {
-  item.onclick = function (event) {
-    mainListingImg.style.backgroundImage = event.target.style.backgroundImage
-  }
-})
+const listingID = new URLSearchParams(window.location.search).get('id')
+const titleOfListing = document.querySelector('#listing-title')
+const listingBidContainerTitle = document.querySelector('#listing-bid-title')
+const listingDescription = document.querySelector('#listing-description p')
+const currentBid = document.querySelector('#current-bid')
+const listingImgMain = document.querySelector('#listing-img-main')
+const listingImgGallery = document.querySelector('#listing-img-gallery')
+
+getListings(API_BASE_URL + GET_LISTING_DETAILS + listingID + '?_seller=true&_bids=true')
+  .then(({title, media, bids, description}) => {
+    let isTitle = title
+    let isDescription = description
+    let listingImg = media[0]
+
+    !isTitle ? isTitle = 'No Title' : null
+    !isDescription ? isDescription = 'No Description' : null
+    !listingImg ? listingImg = listingPlaceholderImg : null
+
+    titleOfListing.innerText = isTitle
+    listingDescription.innerText = isDescription
+    listingBidContainerTitle.innerText = isTitle
+    listingImgMain.style.backgroundImage = `url(${listingImg})`
+
+    currentBid.innerText = 'NO BIDS'
+    if (bids.length) {
+      const highestBid = Math.max(...bids.map(bid => bid.amount))
+      currentBid.innerText = `${highestBid} Credits`
+    }
+    if (media.length > 1) {
+      media.forEach(item => {
+        listingImgGallery.innerHTML +=
+          `<div class="gallery-img cursor-pointer h-14 w-full bg-gray-800 bg-cover bg-top lg:h-20"
+                style="background-image: url(${item})"></div>`
+      })
+    }
+  })
+  .catch(error => {
+    document.querySelector('#listing-details-container').innerHTML = ''
+    titleOfListing.innerHTML = ''
+    showErrorMsg(document.querySelector('#general-error'))
+  })
+  .finally(item => {
+    const galleryImgs = document.querySelectorAll('.gallery-img')
+    galleryImgs.forEach((item) => {
+      item.onclick = function (event) {
+        listingImgMain.style.backgroundImage = event.target.style.backgroundImage
+      }
+    })
+  })
--- src/js/settings/api.js
+++ src/js/settings/api.js
@@ -1,4 +1,5 @@
 const API_BASE_URL = 'https://nf-api.onrender.com/api/v1/auction'
 const GET_LISTINGS = '/listings?_seller=true&_bids=true&sort=created&sortOrder=desc'
+const GET_LISTING_DETAILS = '/listings/'
 
-export {API_BASE_URL, GET_LISTINGS}
+export {API_BASE_URL, GET_LISTINGS, GET_LISTING_DETAILS}
