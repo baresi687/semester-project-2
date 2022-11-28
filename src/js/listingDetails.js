7b72f0d371044351b703aa7f652828694862fa33
hreinn <hreinngy@gmail.com>
2022-11-28 15:54:25 +0100

    Adds time remaining on listing details page

--- src/js/listingDetails.js
+++ src/js/listingDetails.js
@@ -2,35 +2,51 @@ import {API_BASE_URL, GET_LISTING_DETAILS} from "./settings/api";
 import {getListings} from "./settings/getListings";
 import listingPlaceholderImg from "../img/placeholder-image.svg";
 import {showErrorMsg} from "./utils/errorMessages";
+import {DateTime} from "luxon";
 
+const now = DateTime.now()
 const listingID = new URLSearchParams(window.location.search).get('id')
 const titleOfListing = document.querySelector('#listing-title')
 const listingBidContainerTitle = document.querySelector('#listing-bid-title')
 const listingDescription = document.querySelector('#listing-description p')
 const currentBid = document.querySelector('#current-bid')
+const timeLeft = document.querySelector('#bid-remaining')
 const listingImgMain = document.querySelector('#listing-img-main')
 const listingImgGallery = document.querySelector('#listing-img-gallery')
 
 getListings(API_BASE_URL + GET_LISTING_DETAILS + listingID + '?_seller=true&_bids=true')
-  .then(({title, media, bids, description}) => {
+  .then(({title, media, bids, description, endsAt}) => {
+    const listingEndsAt = DateTime.fromISO(endsAt);
+    const diffInDays = listingEndsAt.diff(now, ['days', 'hours', "minutes"]).toObject();
+    const {days, hours, minutes} = diffInDays
+    let timeRemaining = `${days} days, ${hours} hours & ${parseInt(minutes)} minutes`
+
     let isTitle = title
     let isDescription = description
     let listingImg = media[0]
+
+    if (days < 0 || hours < 0 || minutes < 0) {
+      timeRemaining = 'BIDDING HAS ENDED'
+      timeLeft.classList.add('text-red-400')
+      document.querySelector('#bid-btn').disabled = true
+    }
 
     !isTitle ? isTitle = 'No Title' : null
     !isDescription ? isDescription = 'No Description' : null
     !listingImg ? listingImg = listingPlaceholderImg : null
 
-    titleOfListing.innerText = isTitle
-    listingDescription.innerText = isDescription
-    listingBidContainerTitle.innerText = isTitle
+    timeLeft.textContent = timeRemaining
+    titleOfListing.textContent = isTitle
+    listingDescription.textContent = isDescription
+    listingBidContainerTitle.textContent = isTitle
     listingImgMain.style.backgroundImage = `url(${listingImg})`
+    currentBid.textContent = 'NO BIDS'
 
-    currentBid.innerText = 'NO BIDS'
     if (bids.length) {
       const highestBid = Math.max(...bids.map(bid => bid.amount))
-      currentBid.innerText = `${highestBid} Credits`
+      currentBid.textContent = `${highestBid} Credits`
     }
+
     if (media.length > 1) {
       media.forEach(item => {
         listingImgGallery.innerHTML +=
