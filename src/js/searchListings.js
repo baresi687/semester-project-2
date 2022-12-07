e11f5487aa3596d74f00c958b0cd0d122b5cad7f
hreinn <hreinngy@gmail.com>
2022-12-07 20:05:42 +0100

    Sets the right default text color on all pages and fixes margin bottom on search page with no results

--- src/js/searchListings.js
+++ src/js/searchListings.js
@@ -23,7 +23,7 @@ getListings(API_BASE_URL+GET_LISTINGS)
       const html = listingFeedHtml(filteredResponse)
       searchResultsContainer.innerHTML = html.join(' ')
     } else {
-      searchResultsContainer.innerHTML += `<h2 class="text-xl italic font-extralight font-krub mb-4">No Results Found</h2>`
+      searchResultsContainer.innerHTML += `<h2 class="text-xl italic font-extralight font-krub mb-60">No Results Found</h2>`
     }
 
   })
