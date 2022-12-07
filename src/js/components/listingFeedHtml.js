80624f6afd679d77f58b7cb77ed45a5eed291ae2
hreinn <hreinngy@gmail.com>
2022-12-07 20:37:02 +0100

    Fixes indent/padding on Create listing description input. Capitalizes listingFeedHTml titles

--- src/js/components/listingFeedHtml.js
+++ src/js/components/listingFeedHtml.js
@@ -15,13 +15,12 @@ export function listingFeedHtml(arr) {
     !titleOFListing ? titleOFListing = 'No Title' : null
 
     return `<a href="listing-details.html?id=${id}" class="group">
-                <div class="flex flex-col gap-4">
-                  <div class="h-64 w-full bg-cover bg-center"
-                       style="background-image: url(${listingImg})"></div>
-                  <h3 class="text-xl font-medium">${titleOFListing}</h3>
-                  <p>Current Bid: <span class="text-emerald-700 font-bold">${currentBid}</span></p>
-                  <div class="bg-blue-700 text-white text-center w-full rounded py-2 group-hover:bg-blue-600">View Details</div>                 
-                </div>
-              </a>`
+              <div class="flex flex-col gap-4">
+                <div class="h-64 w-full bg-cover bg-center" style="background-image: url(${listingImg})"></div>
+                <h3 class="text-xl font-medium capitalize">${titleOFListing}</h3>
+                <p>Current Bid: <span class="text-emerald-700 font-bold">${currentBid}</span></p>
+                <div class="bg-blue-700 text-white text-center w-full rounded py-2 group-hover:bg-blue-600">View Details</div>                 
+              </div>
+            </a>`
   })
 }
