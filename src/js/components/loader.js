9d886945f91e85c35ff4041e5cdbe401d8700326
hreinn <hreinngy@gmail.com>
2022-12-08 14:39:14 +0100

    Adds loader to Home, Listing details and Search page

--- src/js/components/loader.js
+++ src/js/components/loader.js
@@ -1,3 +1,15 @@
 const buttonProcessing = `<svg id="form-loader" class="absolute top-2.5 left-1 rounded-full border-4 border-slate-400 border-t-slate-100 animate-spin h-5 w-5 ml-3  ..." viewBox="0 0 24 24"></svg>Processing...`
 
-export {buttonProcessing}
+function addLoader(elem) {
+  const loader = `
+   <div id="loader" class="flex justify-center">
+     <svg class="rounded-full border-4 border-slate-200 border-t-slate-700 animate-spin h-8 w-8 mr-6 ..." viewBox="0 0 24 24"></svg>     
+   </div>`
+  elem.insertAdjacentHTML('beforebegin', loader)
+}
+
+function removeLoader() {
+  document.querySelector('#loader').remove()
+}
+
+export {buttonProcessing, addLoader, removeLoader}
--- src/js/settings/getListings.js
+++ src/js/settings/getListings.js
@@ -1,4 +1,7 @@
-export async function getListings(url, options) {
+import {addLoader} from "../components/loader";
+
+export async function getListings(url, options, loader, loaderElem) {
+  loader ? addLoader(loaderElem) : null
   const response = await fetch(url, options)
   return await response.json()
 }
