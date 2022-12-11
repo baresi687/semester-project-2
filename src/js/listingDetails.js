d41d1c38ab9434d6544bd2e1caf74908b1351d03
hreinn <hreinngy@gmail.com>
2022-12-11 18:07:49 +0100

    Improves tablet view, position of bid error messages and limits image gallery to 5 images on listing details page

--- src/js/listingDetails.js
+++ src/js/listingDetails.js
@@ -78,11 +78,11 @@ function getListingDetails() {
 
       if (media.length > 1) {
         document.querySelector('#listing-img').classList.add('gap-6')
-        media.forEach(item => {
-          listingImgGallery.innerHTML +=
-            `<div class="gallery-img cursor-pointer h-14 w-full bg-cover bg-center lg:h-20"
-                style="background-image: url(${item})"></div>`
-        })
+        for (let i = 0; i < media.length; i++) {
+          if (i > 4) {break}
+          listingImgGallery.innerHTML += `<div class="gallery-img cursor-pointer h-14 w-full bg-cover bg-center rounded lg:h-20"
+                                               style="background-image: url(${media[i]})"></div>`
+        }
       }
 
       accessToken.length ? bidOnListingInput.setAttribute('required', '') : null
