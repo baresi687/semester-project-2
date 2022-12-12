a0fb6b0cf26b6c9f22a8de0e00bea0ee9784714b
hreinn <hreinngy@gmail.com>
2022-12-12 12:25:13 +0100

    Fixes '<' in listing title breaking the html

--- src/js/components/listingFeedHtml.js
+++ src/js/components/listingFeedHtml.js
@@ -14,11 +14,12 @@ export function listingFeedHtml(arr) {
     let titleOFListing = title.substring(0, 28)
     !titleOFListing ? titleOFListing = 'No Title' : null
     titleOFListing.length === 28 ? titleOFListing += '..' : null
+    let escapedLessThanTitle = titleOFListing.replace(/</g, "&lt")
 
     return `<a href="listing-details.html?id=${id}" class="group">
               <div class="flex flex-col gap-4">
                 <div class="h-64 w-full bg-cover bg-center rounded" style="background-image: url(${listingImg})"></div>
-                <h3 class="text-xl font-medium capitalize">${titleOFListing}</h3>
+                <h3 class="text-xl font-medium capitalize">${escapedLessThanTitle}</h3>
                 <p>Current Bid: <span class="text-emerald-700 font-bold">${currentBid}</span></p>
                 <div class="bg-blue-700 text-white text-center w-full rounded py-2 group-hover:bg-blue-600">View Details</div>                 
               </div>
