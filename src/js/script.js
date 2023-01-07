b9ca78f2bb5ae2544e42d6f1d43ec18fbc2b601b
hreinn <hreinngy@gmail.com>
2023-01-07 03:03:04 +0100

    Changes Hamburger menu elements to button > svg for improved accessibility. Fixes Headline icons to be included in build

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,4 @@
 import '../css/style.css';
-import openMenu from '../img/hamburger-menu.svg';
-import closeMenu from '../img/close-hamburger-menu.svg';
 import { createNavHeaderBar } from './components/createNavHeaderBar';
 import { clearStorage } from './utils/storage';
 
@@ -11,26 +9,33 @@ const searchBtn = document.querySelector('#search-btn');
 
 createNavHeaderBar();
 
-hamburgerBtn.onclick = function () {
-  hamburgerMenu.classList.toggle('-translate-x-0');
-  if (hamburgerMenu.classList.contains('-translate-x-0')) {
-    hamburgerBtn.querySelector('img').src = closeMenu;
-  } else {
-    hamburgerBtn.querySelector('img').src = openMenu;
-  }
-};
-
 window.addEventListener('click', function (event) {
   if (
-    event.target.parentElement !== document.querySelector('#menu') &&
-    event.target !== hamburgerBtn.querySelector('img') &&
-    hamburgerMenu.classList.contains('-translate-x-0')
+    event.target.closest('#hamburger-btn') &&
+    !hamburgerMenu.classList.contains('-translate-x-0')
+  ) {
+    hamburgerMenu.classList.toggle('-translate-x-0');
+    hamburgerBtn.querySelector('svg').classList.toggle('rotate-90');
+    hamburgerBtn
+      .querySelector('path')
+      .setAttribute(
+        'd',
+        'M24 2.41714L21.5829 0L12 9.58286L2.41714 0L0 2.41714L9.58286 12L0 21.5829L2.41714 24L12 14.4171L21.5829 24L24 21.5829L14.4171 12L24 2.41714Z'
+      );
+  } else if (
+    hamburgerMenu.classList.contains('-translate-x-0') &&
+    !event.target.closest('#menu')
   ) {
     hamburgerMenu.classList.remove('-translate-x-0');
-    hamburgerBtn.querySelector('img').src = openMenu;
+    hamburgerBtn.querySelector('svg').classList.remove('rotate-90');
+    hamburgerBtn
+      .querySelector('path')
+      .setAttribute(
+        'd',
+        'M0 24.35H24V20.35H0V24.35ZM0 14.35H24V10.35H0V14.35ZM0 0.349976V4.34998H24V0.349976H0Z'
+      );
   }
 });
-
 searchBtn.addEventListener('click', function () {
   if (searchInput.value.trim()) {
     location.href = `../search-listings.html?search=${searchInput.value}`;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         archivo: ['Archivo', 'sans-serif'],
         inter: ['Inter', 'sans-serif'],
       },
+      content: {
+        checkMark: 'url("/img/checkmark.svg")',
+      },
     },
   },
   plugins: [],
